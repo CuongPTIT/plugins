efe7c1377ed60c364b2f3dc5c1bb1d41aff21149
Anuar Otynshin <strigeandro@gmail.com>
2021-01-14 14:39:56 +0600

    update ground overlays example page, add more parameters to play with

--- packages/google_maps_flutter/google_maps_flutter/android/src/main/java/io/flutter/plugins/googlemaps/GoogleMapController.java
+++ packages/google_maps_flutter/google_maps_flutter/android/src/main/java/io/flutter/plugins/googlemaps/GoogleMapController.java
@@ -758,7 +758,7 @@ final class GoogleMapController
   }
 
   private void updateInitialGroundOverlays() {
-    groundOverlaysController.addOverlays(initialGroundOverlays);
+    groundOverlaysController.addGroundOverlays(initialGroundOverlays);
   }
 
   @SuppressLint("MissingPermission")
